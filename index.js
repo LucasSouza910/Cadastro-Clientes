1dfc310a4368ea3174cf62350e8ee8645a323077
LucasSouza910 <lucasaguiarpl@outlook.com>
2022-05-13 20:22:48 -0300

    commit

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ app.set('view engine', 'ejs')
 var path = require('path');
 app.set('views', path.join(__dirname, '/view/'));
 
+bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true}));
+
 var consign = require('consign');
 consign().include('controller/routes',).into(app);

a3e7b731ef7153114d44274127e26b8de392b3de
LucasSouza910 <lucasaguiarpl@outlook.com>
2022-05-20 20:32:06 -0300

    commit

--- model/repositories/usuarioBD.js
+++ model/repositories/usuarioBD.js
@@ -6,3 +6,13 @@ async function selectUsuario(){
     const [rows] = await conn.query('SELECT * FROM usuario;');
     return rows;
 }
+
+async function getUsuarioId(id){
+    const conn = await usuarioBD.connect();
+    const sql = 'SELECT * FROM usuario WHERE id =?';
+    const values = [id];
+    const [rows] = await conn.query(sql, values);
+    if(rows.length > 0)
+        return rows[0];
+    else return null;
+}

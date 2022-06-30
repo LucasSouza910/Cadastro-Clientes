da3c19e0a934e45b2d9cdaa466e5482b68d57b2b
LucasSouza910 <lucasaguiarpl@outlook.com>
2022-06-29 23:19:42 -0300

    commit

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ app.use(bodyParser.urlencoded({ extended: true}));
 var consign = require('consign');
 consign().include('controller/routes',).into(app);
 
+app.get("/", function(req, resp){
+    resp.send("Bem-Vindo ao meu app");
+})
+
+app.get("/contato", function(req, resp){
+    resp.send("Página de contato");
+})
+
+app.get("/produto", function(req, resp){
+    resp.send("Página de produto");
+})
+
+app.get("/dados/:nome/:cargo", function(req, resp){
+    resp.send("<h1>Olá Sr(a)."+req.params.nome+ "</h1><h2>Seu cargo é " +req.params.cargo+ "</h2>");
+})
+
 app.listen(8081, function(){
     console.log("Servidor funcionando na url http://localhost:8081");
 })
--- model/services/dborm.js
+++ model/services/dborm.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const sequeliza = new Sequelize('agendamento', 'root', 'root',
+const sequelize = new Sequelize('agendamento', 'root', 'root',
                     {dialect: 'mysql', host:'localhost', port:3306});
 module.exports = sequelize
